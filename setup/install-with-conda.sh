202172850f826e7d2d46ae2a7cb99b9217f3f5d4
Piotr Szul <piotr.szul@csiro.au>
2019-05-24 17:58:35 +0200

    Added conda install script

---
+++ setup/install-with-conda.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+PWD=$(cd "`dirname $0`"/..; pwd)
+TRIBES_ENV=tribes
+
+conda create -y -n ${TRIBES_ENV} python=3.6
+source activate ${TRIBES_ENV}
+conda install -y --clobber -c bioconda -c aehrc --file "${PWD}/setup/requirements.txt"
+pip install 'snakemake>=5.4'
+Rscript --vanilla -e "install.packages('${PWD}/R/tribes.tools',repos=NULL)"
+cat > "${HOME}/.tribesrc" <<EOF
+source activate ${TRIBES_ENV}
+EOF
