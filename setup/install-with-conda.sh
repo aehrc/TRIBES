4e54ab5590639283d0e7c24ad6d1032037fdc698
Natalie.Twine <natalie.twine@csiro.au>
2019-05-28 15:34:19 +1000

    added conda-forge channel to setup

--- setup/install-with-conda.sh
+++ setup/install-with-conda.sh
@@ -6,7 +6,7 @@ TRIBES_ENV=tribes
 
 conda create -y -n ${TRIBES_ENV} python=3.6
 source activate ${TRIBES_ENV}
-conda install -y --clobber -c bioconda -c aehrc --file "${PWD}/setup/requirements.txt"
+conda install -y --clobber -c conda-forge -c bioconda -c aehrc --file "${PWD}/setup/requirements.txt"
 pip install 'snakemake>=5.4'
 Rscript --vanilla -e "install.packages('${PWD}/R/tribes.tools',repos=NULL)"
 cat > "${HOME}/.tribesrc" <<EOF
