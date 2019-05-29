123b80a3673219e4ac4fc9db89f4f0b21470e4b8
Piotr Szul <piotr.szul@data61.csiro.au>
2019-05-30 02:15:41 +1000

    Added detection of conda activation command

--- setup/install-with-conda.sh
+++ setup/install-with-conda.sh
@@ -3,9 +3,14 @@ set -e
 
 PWD=$(cd "`dirname $0`"/..; pwd)
 TRIBES_ENV=tribes
+CONDA_ACTIVATE="conda"
+if [ -n "$(which activate 2> /dev/null; echo -n)" ]; then
+    CONDA_ACTIVATE="source"
+fi
+echo $CONDA_ACTIVATE
 conda env create --force -n ${TRIBES_ENV} -f "${PWD}/setup/environment.yaml"
-source activate ${TRIBES_ENV}
+${CONDA_ACTIVATE} activate ${TRIBES_ENV}
 Rscript --vanilla -e "install.packages('${PWD}/R/tribes.tools',repos=NULL)"
 cat > "${HOME}/.tribesrc" <<EOF
-source activate ${TRIBES_ENV}
+${CONDA_ACTIVATE} activate ${TRIBES_ENV}
 EOF
