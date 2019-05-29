829dcc3cb5d89db280e55449e4173c0743a6713a
Piotr Szul <piotr.szul@data61.csiro.au>
2019-05-30 06:38:03 +1000

    Fixed conda activation

--- setup/install-with-conda.sh
+++ setup/install-with-conda.sh
@@ -3,14 +3,10 @@ set -e
 
 PWD=$(cd "`dirname $0`"/..; pwd)
 TRIBES_ENV=tribes
-CONDA_ACTIVATE="conda"
-if [ -n "$(which activate 2> /dev/null; echo -n)" ]; then
-    CONDA_ACTIVATE="source"
-fi
-echo $CONDA_ACTIVATE
+CONDA_BASE="$(conda info --base)"
 conda env create --force -n ${TRIBES_ENV} -f "${PWD}/setup/environment.yaml"
-${CONDA_ACTIVATE} activate ${TRIBES_ENV}
+source "${CONDA_BASE}/bin/activate" ${TRIBES_ENV} 
 Rscript --vanilla -e "install.packages('${PWD}/R/tribes.tools',repos=NULL)"
 cat > "${HOME}/.tribesrc" <<EOF
-${CONDA_ACTIVATE} activate ${TRIBES_ENV}
+source "${CONDA_BASE}/bin/activate" ${TRIBES_ENV} 
 EOF
