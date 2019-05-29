8957f83164caa6023bc4df1d97b23aa487b30dbb
Piotr Szul <piotr.szul@csiro.au>
2019-05-29 16:37:59 +0200

    Updated setup to use conda env file and added dependency on htslib (tabix and bgzip)

--- setup/install-with-conda.sh
+++ setup/install-with-conda.sh
@@ -3,11 +3,8 @@ set -e
 
 PWD=$(cd "`dirname $0`"/..; pwd)
 TRIBES_ENV=tribes
-
-conda create -y -n ${TRIBES_ENV} python=3.6
+conda env create --force -n ${TRIBES_ENV} -f "${PWD}/setup/environment.yaml"
 source activate ${TRIBES_ENV}
-conda install -y --clobber -c bioconda -c aehrc --file "${PWD}/setup/requirements.txt"
-pip install 'snakemake>=5.4'
 Rscript --vanilla -e "install.packages('${PWD}/R/tribes.tools',repos=NULL)"
 cat > "${HOME}/.tribesrc" <<EOF
 source activate ${TRIBES_ENV}
