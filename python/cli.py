657ade95167bb675ade5781406a2b2852e0ac14b
Piotr Szul <piotr.szul@data61.csiro.au>
2019-11-15 13:12:25 +1100

    Added pure sql version

--- python/cli.py
+++ python/cli.py
@@ -14,11 +14,25 @@ def cli():
 def with_spark(func):
     def do(**kwargs):
         click.echo("Creating spark session")
-        conf = SparkConf()
+        conf = SparkConf().set('spark.driver.memory','10G')
         spark = SparkSession.Builder().config(conf=conf).getOrCreate()
         return func(spark,spark.sparkContext, **kwargs)
     return update_wrapper(do, func)
 
+
+
+GERMLINE_SCHEMA_FIX = StructType()\
+    .add("Fam1_Id1", StringType()) \
+    .add("Fam2_Id2", StringType()) \
+    .add("Chr", StringType()) \
+    .add("SegStart_SegEnd", StringType()) \
+    .add("SnpStart_SnpEnd", StringType()) \
+    .add("NoSnp", IntegerType()) \
+    .add("Length", DoubleType()) \
+    .add("Unit", StringType()) \
+    .add("NoMismatch", IntegerType()) \
+    .add("Res1", StringType()) \
+    .add("Res2", StringType()) 
 
 GERMLINE_SCHEMA = StructType()\
     .add("Fam1", StringType()) \
@@ -50,6 +64,23 @@ def ibd(spark, sc, input):
         .select(*[col(f.name).cast(f.dataType) for f in GERMLINE_SCHEMA.fields])
     ibd_per_pair = df.groupBy(col('Id1'), col('Id2')).sum('Length')
     ibd_per_pair.show()
+@cli.command()
+@click.argument('input')
+#@click.argument('output')
+@with_spark
+def ibd2(spark, sc, input):
+    """ Convert the full graph to the plain edges representation with numerical ids
+        to be used by dga or as an input for further conversion to metis
+    """
+    raw_df = spark.read.csv(input, sep='\t', schema=GERMLINE_SCHEMA_FIX)
+    df = raw_df.select(
+        split(col('Fam1_Id1'),' ')[1].alias('Id1'),
+        split(col('Fam2_Id2'),' ')[1].alias('Id2'),
+        col('Length')
+    )
+    ibd_per_pair = df.groupBy(col('Id1'), col('Id2')).sum('Length')
+    ibd_per_pair.show()
+
 
 if __name__ == '__main__':
     cli()
